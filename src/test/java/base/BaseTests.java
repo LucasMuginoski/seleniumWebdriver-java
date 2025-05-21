e3f5911ec6f3fbd8f60c72e72fe40497c45a096e
Lucas Muginoski <lucasmmuginoski@gmail.com>
2025-05-21 11:46:36 -0300

    tests: nagivations + setup: take screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,13 +1,19 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
@@ -29,6 +35,29 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            //cast drive into new class
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("src/main/resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        }
+        else {
+            System.out.println("Test has passed, screenshot not needed.");
+        }
+
+    }
+
+    public WindowManager getWindowManager(){
+        return new WindowManager(driver);
     }
 
 }
